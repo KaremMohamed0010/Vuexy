19a06a4c9285a08fe6afcc1e7c081867901ef441
KaremMohamed0010 <karem.mohamed0010@gmail.com>
2021-12-26 15:45:49 +0200

    first commit

---
+++ src/views/pages/profile/ProfileAbout.vue
@@ -0,0 +1,26 @@
+<template>
+  <b-card>
+    <!-- about -->
+    <div :class="index ? 'mt-2' : ''">
+      <h5 class="text-capitalize mb-75">tes</h5>
+      <b-card-text> fs </b-card-text>
+    </div>
+  </b-card>
+</template>
+
+<script>
+import { BCard, BCardText } from "bootstrap-vue";
+
+export default {
+  components: {
+    BCard,
+    BCardText,
+  },
+  props: {
+    aboutData: {
+      type: Object,
+      default: () => {},
+    },
+  },
+};
+</script>
